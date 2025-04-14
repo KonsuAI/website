0b4e825baeeec559d708405f9cc40a969593efa1
Wilf Silver <git@wilfsilver.co.uk>
2025-04-14 16:13:45 +0100

    Add the itinerary page

---
+++ src/lib/components/trips/itinerary/Item.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let { imageUrl, title, meta }: { imageUrl: string; title: string; meta: string } = $props();
+	let loaded = $state(false);
+</script>
+
+<button
+	class="relative h-full w-84 snap-center overflow-hidden rounded-3xl text-left"
+	onclick={() => goto('/app/journey')}
+>
+	<img
+		class="h-full w-full object-cover"
+		src={imageUrl}
+		alt={title}
+		onload={() => (loaded = true)}
+	/>
+	{#if loaded}
+		<div class="bg-konsu-dark-00/35 absolute top-0 left-0 h-full w-full">
+			<div class="grid h-full grid-cols-1 p-4">
+				<div class="self-end">
+					<h3 class="text-2xl font-bold">{title}</h3>
+					<div class="meta">{meta}</div>
+				</div>
+			</div>
+		</div>
+	{/if}
+</button>
---
+++ src/routes/app/itinerary/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import AiInput from '$lib/components/AiInput.svelte';
+	import Page from '$lib/components/Page.svelte';
+	import Item from '$lib/components/trips/itinerary/Item.svelte';
+</script>
+
+<Page
+	class="grid h-screen max-h-screen grid-rows-[max-content_1fr_max-content]"
+	title="Here's your plan!"
+>
+	<div class="grid snap-x snap-mandatory grid-flow-col gap-4 overflow-x-scroll">
+		<Item
+			title="Your Holiday to Friuli-Venezia Giulia, Italy"
+			imageUrl="https://s3-alpha-sig.figma.com/img/17bf/c79b/0ead6ea4d0eb12198e00da2862767ce8?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lkFdhtGFY6dZkNL3aiK1r7J6rWPea0AAcGhm98v0jJiQ-K0uSIARM9zo-v~~S9VWssgOMz5OlaTd183-6tl0hg9w0PAXzaO9W6hbx3gLikbHQwDv79VUtXC26mtu7ogS1JfdQkm2PkO2~94pGG8PHvkRyqay66qTmr8Xgq-kJg6TnAzqdeT2rW93XXAEaSZgH4V4t9niQ5HIlvb4gAYMamVyqbgHffo52NcPzT5krNJcdKeOZTMOIhL2h~1i1dm~ev4KQauNaxA4CdNd88Bhu-IhDHFdR9qTK1MtdLKqghZwptQmAFdhg5MLuNXqdMEDsgNicKNtyR0Blwsv2Dbs3g__"
+			meta="Swipe left for Day 1."
+		/>
+
+		<Item
+			title="Day one: Visiting the castle"
+			imageUrl="https://s3-alpha-sig.figma.com/img/c93f/e9b5/5678309fc5488cb3881824f2fa39b63d?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Kj3rr5C-Ce9jrtDM8ytCwejY9VCLFz2fIZv0TaYzMgku1uxxW4KRGMffAD1t16SpUfkQV0PdEgXn3eS9-V8EnfKfnh0GRiOSMQpHN9pXWz0pxmSSyAFniQ1s2Cr~FBp~a8s2sCQ~wBsYhWYXP1-3pD9qptgjxwwFS6k5BYb~UgYQsNwlrlpJx3u~D6HHSj3JiBAdGLrLiRz8bNmc0kgZUgW3wb5rdvp8In5RlbLAlNbZsvVT-jfOzJl6-v8fqc-J6fvZVqZ4F7HkGPjz91ay4TlJrJ-vziQukrm1BeXjv~b7W4Vp7RkzOd-9IwF~0Ae8h2zIiKv9WnXbgDbA6f2RDA__"
+			meta="Explore the beauty of Castello di Udine after dropping your bags off at the hotel, with Wine tasting in the evening at Vineria La Botte"
+		/>
+
+		<Item
+			title="Day two: Explore the wilds"
+			imageUrl="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/68/0a/37/caption.jpg"
+			meta="Packing in the morning for a beautiful day trip to the Nation Park"
+		/>
+
+		<Item
+			title="Day three: Take a breath"
+			imageUrl="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/69/2e/a8/caption.jpg"
+			meta="Leaving your hotel in the morning and enjoying your last day by strolling around the city before coming home!"
+		/>
+	</div>
+
+	<AiInput
+		placeholder="Wanna change something?"
+		onsubmit={function (e) {
+			e.preventDefault();
+			console.log(this);
+		}}
+	/>
+</Page>
